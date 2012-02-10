16a4a754a5a1ce101f53435b012a9503f9e20b04
KASHIO Takaaki <t.kashio@gmail.com>
2012-02-10 17:25:13 +0900

    固定ページに子ページの内容を表示するように変更

--- sidebar.php
+++ sidebar.php
@@ -7,11 +7,15 @@
  */
 ?>
 
-<aside id="top-sidebar" class="sidebar">
+<aside id="sidebar" class="sidebar">
 	<!-- Googleカレンダー -->
-	<iframe src="https://www.google.com/calendar/embed?showNav=0&amp;showTabs=0&amp;showTz=0&amp;mode=AGENDA&amp;height=300&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=5qo3kpsp1l1ni5csrbk342kvuo%40group.calendar.google.com&amp;color=%23853104&amp;ctz=Asia%2FTokyo" style=" border:solid 1px #777 " width="230" height="300" frameborder="0" scrolling="no"></iframe>
+	<?php if (is_home()) : ?>
+		<iframe src="https://www.google.com/calendar/embed?showTz=0&amp;height=300&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=5qo3kpsp1l1ni5csrbk342kvuo%40group.calendar.google.com&amp;color=%23853104&amp;ctz=Asia%2FTokyo" style=" border-width:0 " width="390" height="300" frameborder="0" scrolling="no"></iframe>
+	<?php else : ?>
+		<iframe src="https://www.google.com/calendar/embed?showNav=0&amp;showTabs=0&amp;showTz=0&amp;mode=AGENDA&amp;height=300&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=5qo3kpsp1l1ni5csrbk342kvuo%40group.calendar.google.com&amp;color=%23853104&amp;ctz=Asia%2FTokyo" style=" border-width:0 " width="190" height="300" frameborder="0" scrolling="no"></iframe>
+	<?php endif; ?>
 	<div id="toto-logo">
-		<img src="img/toto_logo.png" alt="toto" />
+		<img src="<?php bloginfo('stylesheet_directory'); ?>/images/toto_logo.png" alt="toto" />
 		<p>この事業はスポーツ振興くじの助成を受けて実施しています。</p>
 	</div>
 </aside>
