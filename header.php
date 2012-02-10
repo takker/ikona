1b9532dc1dc0571601095dff67e5ffb2568fa2de
KASHIO Takaaki <t.kashio@gmail.com>
2012-02-10 13:15:32 +0900

    LESSの読み込み

--- header.php
+++ header.php
@@ -4,6 +4,10 @@
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<title><?php wp_title(); ?><?php bloginfo('name'); ?></title>
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" media="screen" />
+	<!-- LESSの読み込み -->
+	<link rel="stylesheet/less" href="<?php bloginfo('stylesheet_directory'); ?>/css/styles.less" />
+	<script src="http://lesscss.googlecode.com/files/less-1.2.1.min.js"></script>
+
 	<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
 	<link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
 	<link rel="alternate" type="application/atom+xml" title="Atom 0.3" href="<?php bloginfo('atom_url'); ?>" />
