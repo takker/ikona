591886e2309d4855e834eeaa2eb653098ede67ce
KASHIO Takaaki <t.kashio@gmail.com>
2012-02-13 00:59:41 +0900

    singleの修正

--- single.php
+++ single.php
@@ -2,11 +2,17 @@
 
 <?php if (have_posts()) : ?>
 	<?php while (have_posts()) : the_post(); ?>
-		<div class="post">
-			<a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-			<?php the_date_xml(); ?>
+		<div class="nav-previous"><?php previous_post_link() ?></div>
+		<div class="nav-next"><?php next_post_link() ?></div>
+		<article class="post">
+			<h2><?php the_title(); ?></h2>
+			<?php the_date(); ?>
 			<?php the_content('続きを読む'); ?>
-		</div>
+		</article>
+		<div>カテゴリ:<?php the_category(); ?></div>
+		<div><?php the_tags(); ?></div>
+		<h3 id="comments-title"><?php comments_number(); ?></h3>
+		<?php comment_form(array('comment_notes_after' => '')); ?>
 	<?php endwhile; ?>
 <?php else : ?>
 	<h2>Not Found</h2>
